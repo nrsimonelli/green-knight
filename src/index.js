9c71f3b89158e848f9c6f24ffe96d8e643613977
nrsimonelli <nrsimonelli@gmail.com>
2021-10-13 20:30:07 -0500

    react-router-dom wrapped browser router around app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './components/ThemeToggle/ThemeContext';
 
 ReactDOM.render(
   <ThemeProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ThemeProvider>,
   document.getElementById('root')
 );
